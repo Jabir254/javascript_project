ba449fd5323fd81e4adb61b287be274ba0a0f153
jabir254 <jabirkullow@gmail.com>
2023-08-13 11:02:22 +0300

    spreading

--- learnin_react/learni.js
+++ learnin_react/learni.js
@@ -1,15 +1,15 @@
 const tahoe = {
 	mountains: ["freel", "Rose", "Tallac", "Rubicon", "Silver"],
-	print: function(delay = 1000){
+	print: function (delay = 1000) {
 		setTimeout(() => {
 			console.log(this.mountains.join(", "));
 		}, delay);
-	}
+	},
 };
 
 //tahoe.print();
 
-const add = (x= 5, y=10) => console.log(x+y);
+const add = (x = 5, y = 10) => console.log(x + y);
 //add();
 
 // objects and arrays
@@ -20,8 +20,19 @@ const sandwich = {
 	bread: "dutch crunch",
 	meat: "tuna",
 	cheese: "swiss",
-	toppings: ["lettuce", "tomato", "Mustard"]
+	toppings: ["lettuce", "tomato", "Mustard"],
 };
 
-const{ bread, meat } = sandwich;
-console.log(bread,meat);
+const { bread, meat } = sandwich;
+//console.log(bread, meat);
+
+function directions(...args) {
+	let [start, ...remaining] = args;
+	let [finish, ...stops] = remaining.reverse();
+	console.log(`drive through ${args.length} towns`);
+	console.log(`start in ${start}`);
+	console.log(`the destination is ${finish}`);
+	console.log(`stopping ${stops.length} times in between`);
+	console.log(`stopping towns are ${stops} `);
+}
+directions("Truckee", "Tahoe City", "Sunnyside", "Homewood", "Tahoma");
