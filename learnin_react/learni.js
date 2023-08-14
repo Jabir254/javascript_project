46d3c712ac85f8c288b22e576385c00a2c683ec5
jabir254 <jabirkullow@gmail.com>
2023-08-14 11:16:15 +0300

    updated

--- learnin_react/learni.js
+++ learnin_react/learni.js
@@ -194,24 +194,13 @@ const addColor = function (title, colors) {
 //const highSchools = schools.map((school) => `${school} High School`);
 //console.log(highSchools.join("\n"));
 
-let schools = [
-	{ name: "Yorktown" },
-	{ name: "Stratford" },
-	{ name: "Washington & Liberty" },
-	{ name: "Wakefield" },
-];
-let updatedSchools = editName("Stratford", "HB Woodlawn", schools);
-console.log(updatedSchools[1]); // { name: "HB Woodlawn" }
-console.log(schools[1]); // { name: "Stratford" }
-//
-const editName = (oldName, name, arr) =>
-	arr.map((item) => {
-		if (item.name === oldName) {
-			return {
-				...item,
-				name,
-			};
-		} else {
-			return item;
-		}
-	});
+const ages = [21, 18, 42, 40, 64, 63, 34];
+
+const maxAge = ages.reduce((max, age) => {
+	console.log(`${age} > ${max} = ${age > max}`);
+	if (age > max) {
+		return age;
+	} else {
+		return max;
+	}
+}, 0);
