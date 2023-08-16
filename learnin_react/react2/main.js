a4b5b275d459fd00abeecb79775990725f7c679a
jabir254 <jabirkullow@gmail.com>
2023-08-16 11:28:57 +0300

    modified

--- learnin_react/react2/main.js
+++ learnin_react/react2/main.js
@@ -1,25 +1,76 @@
-const secretIngredients = [
-    "1 cup unsalted butter",
-    "1 cup crunchy peanut butter",
-    "1 cup brown sugar",
-    "1 cup white sugar",
-    "2 eggs",
-    "2.5 cups all purpose flour",
-    "1 teaspoon baking powder",
-    "0.5 teaspoon salt"
+const data = [
+    {
+        name: "Baked Salmon",
+        ingredients: [
+            { name: "Salmon", amount: 1, measurement: "l lb" },
+            { name: "Pine Nuts", amount: 1, measurement: "cup" },
+            { name: "Butter Lettuce", amount: 2, measurement: "cups" },
+            { name: "Yellow Squash", amount: 1, measurement: "med" },
+            { name: "Olive Oil", amount: 0.5, measurement: "cup" },
+            { name: "Garlic", amount: 3, measurement: "cloves" }
+        ],
+        steps: [
+            "Preheat the oven to 350 degrees.",
+            "Spread the olive oil around a glass baking dish.",
+            "Add the yellow squash and place in the oven for 30 mins.",
+            "Add the salmon, garlic, and pine nuts to the dish.",
+            "Bake for 15 minutes.",
+            "Remove from oven. Add the lettuce and serve."
+        ]
+    },
+    {
+        name: "Fish Tacos",
+        ingredients: [
+            { name: "Whitefish", amount: 1, measurement: "l lb" },
+            { name: "Cheese", amount: 1, measurement: "cup" },
+            { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
+            { name: "Tomatoes", amount: 2, measurement: "large" },
+            { name: "Tortillas", amount: 3, measurement: "med" }
+        ],
+        steps: [
+            "Cook the fish on the grill until hot.",
+            "Place the fish on the 3 tortillas.",
+            "Top them with lettuce, tomatoes, and cheese."
+        ]
+    }
 ];
 
-function IngredientsList(props) {
-    return React.createElement(
-        "ul",
-        { className: "ingredients" },
-        props.items.map((ingredient, i) =>
-            React.createElement("li", { key: i }, ingredient)
-        )
+function Recipe({ name, ingredients, steps }) {
+    return (
+        <section id={name.toLowerCase().replace(/ /g, "-")}>
+            <h1>{name}</h1>
+            <ul className="ingredients">
+                {ingredients.map((ingredient, i) => (
+                    <li key={i}>{ingredient.name}</li>
+                ))}
+            </ul>
+            <section className="instructions">
+                <h2>Cooking Instructions</h2>
+                {steps.map((step, i) => (
+                    <p key={i}>{step}</p>
+                ))}
+            </section>
+        </section>
+    );
+}
+
+function Menu({ title, recipes})
+{
+    return (
+        <article>
+            <header>
+                <h1>{title}</h1>
+            </header>
+            <div className="recipe">
+                {recipes.map((recipe, i) =>(
+                    <recipe key={i} {...recipe} />
+                ))}
+            </div>
+        </article>
     );
 }
 
 ReactDom.render(
-    React.createElement(IngredientsList, { items: secretIngredients }, null),
+    <menu recipes={data} title="Delicious Recipes" />,
     document.getElementById("root")
 );
