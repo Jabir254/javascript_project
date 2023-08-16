46b6ef92fe0b2e02a75ee58fce2bc835999c2fe8
jabir254 <jabirkullow@gmail.com>
2023-08-16 10:10:08 +0300

    added js files and updated html

---
+++ learnin_react/react2/main.js
@@ -0,0 +1,3 @@
+const dish = React.createElement("h1", null, "Baked Salmon");
+const dessert = React.createElement("h2", null, "Coconut Cream Pie");
+ReactDOM.render([dish, dessert], document.getElementById("root"));
