d440258f0107bbff392a9041a83c4ffad6c8ad68
jabir254 <jabirkullow@gmail.com>
2023-08-16 10:58:23 +0300

    updated

--- learnin_react/react2/main.js
+++ learnin_react/react2/main.js
@@ -1,50 +1,25 @@
-const dish = React.createElement("h1", null, "Baked Salmon");
-const dessert = React.createElement("h2", null, "Coconut Cream Pie");
-ReactDOM.render([dish, dessert], document.getElementById("root"));
+const secretIngredients = [
+    "1 cup unsalted butter",
+    "1 cup crunchy peanut butter",
+    "1 cup brown sugar",
+    "1 cup white sugar",
+    "2 eggs",
+    "2.5 cups all purpose flour",
+    "1 teaspoon baking powder",
+    "0.5 teaspoon salt"
+];
 
-const list = React.createElement(
-    "ul",
-    null,
-    React.createElement("li", null, "2 lb salmon"),
-    React.createElement("li", null, "5 sprigs fresh rosemary"),
-    React.createElement("li", null, "2 tablespoons olive oil"),
-    React.createElement("li", null, "2 small lemons"),
-    React.createElement("li", null, "1 teaspoon kosher salt"),
-    React.createElement("li", null, "4 cloves of chopped garlic")
-);
-console.log(list);
-
-React.createElement(
-    "section",
-    { id: "baked-salmon" },
-    React.createElement("h1", null, "Baked Salmon"),
-    React.createElement(
+function IngredientsList(props) {
+    return React.createElement(
         "ul",
         { className: "ingredients" },
-        React.createElement("li", null, "2 lb salmon"),
-        React.createElement("li", null, "5 sprigs fresh rosemary"),
-        React.createElement("li", null, "2 tablespoons olive oil"),
-        React.createElement("li", null, "2 small lemons"),
-        React.createElement("li", null, "1 teaspoon kosher salt"),
-        React.createElement("li", null, "4 cloves of chopped garlic")
-    ),
-    React.createElement(
-        "section",
-        { className: "instructions" },
-        React.createElement("h2", null, "Cooking Instructions"),
-        React.createElement("p", null, "Preheat the oven to 375 degrees."),
-        React.createElement("p", null, "Lightly coat aluminum foil with oil."),
-        React.createElement("p", null, "Place salmon on foil."),
-        React.createElement(
-            "p",
-            null,
-            "Cover with rosemary, sliced lemons, chopped garlic."
-        ),
-        React.createElement(
-            "p",
-            null,
-            "Bake for 15-20 minutes until cooked through."
-        ),
-        React.createElement("p", null, "Remove from oven.")
-    )
+        props.items.map((ingredient, i) =>
+            React.createElement("li", { key: i }, ingredient)
+        )
+    );
+}
+
+ReactDom.render(
+    React.createElement(IngredientsList, { items: secretIngredients }, null),
+    document.getElementById("root")
 );
