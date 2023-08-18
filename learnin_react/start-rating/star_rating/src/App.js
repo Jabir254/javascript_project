79a0bcf2d65868e201bcb24652c1f4dcd1863644
jabir254 <jabirkullow@gmail.com>
2023-08-18 21:31:21 +0300

    use of state Hooks

--- learnin_react/start-rating/star_rating/src/App.js
+++ learnin_react/start-rating/star_rating/src/App.js
@@ -1,22 +1,22 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating({ totalStars = 5 }) {
-  const [selectedStars] = useState(4);
-  return (
-    <>
-      {createArray(totalStars).map((n, i) =>(
-        <Star key={i} selected={selectedStars > i} />
-      ))}
-      <p>
-        {selectedStars} of {totalStars} stars
-      </p>
-    </>
-  );
- }
+	const [selectedStars] = useState(4);
+	return (
+		<>
+			{createArray(totalStars).map((n, i) => (
+				<Star key={i} selected={selectedStars > i} />
+			))}
+			<p>
+				{selectedStars} of {totalStars} stars
+			</p>
+		</>
+	);
+}
 
-const Star = ({ selected = false, onSelect = f => f }) => (
-  <FaStar color={selected ? "red" : "grey" } onClick={onSelect}/>
+const Star = ({ selected = false, onSelect = (f) => f }) => (
+	<FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
-const createArray = length => [...Array(length)];
+const createArray = (length) => [...Array(length)];
