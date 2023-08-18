c8e3729d51dc93e54712bef8a4e8698c9fd07380
jabir254 <jabirkullow@gmail.com>
2023-08-18 21:22:27 +0300

    added createArray

--- learnin_react/start-rating/star_rating/src/App.js
+++ learnin_react/start-rating/star_rating/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FaStar } from "react-icons/fa";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function StarRating({ totalStars = 5 }) {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
+ }
 
-export default App;
+const Star = ({ selected = false }) => (
+  <FaStar color={selected ? "red" : "grey" } />
+)
+
+const createArray = length => [...Array(length)];
