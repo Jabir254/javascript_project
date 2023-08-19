da78569a6f821dcb0cab6b06633a8fd3630e991b
jabir254 <jabirkullow@gmail.com>
2023-08-19 10:55:03 +0300

    building the tree

--- learnin_react/start-rating/star_rating/src/App.js
+++ learnin_react/start-rating/star_rating/src/App.js
@@ -1,25 +1,15 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating({ style = {}, totalStars = 10 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return (
-    <div style={{ padding: "5px", ...style }}>
-      {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
-        />
-      ))}
-
-      <p>
-        {selectedStars} of {totalStars} stars
-      </p>
-    </div>
-  );
+export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+	return (
+		<>
+			{createArray(totalStars).map((n, i) => (
+				<Star key={i} selected={selectedStars > i} />
+			))}
+			<p>
+				{selectedStars} of {totalStars} stars
+			</p>
+		</>
+	);
 }
-
-const Star = ({ selected = false, onSelect = (f) => f }) => (
-  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
-);
-
-const createArray = (length) => [...Array(length)];
