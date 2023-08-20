d11d5066744079041da8eff3b0156c706ca540d7
jabir254 <jabirkullow@gmail.com>
2023-08-20 11:03:20 +0300

    updated

--- learnin_react/start-rating/star_rating/src/ColorList.js
+++ learnin_react/start-rating/star_rating/src/ColorList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
-	if (!colors.length) return <div>No Colors Listed.</div>;
+export default function ColorList({
+	colors = [],
+	onRemove = (f) => f,
+	onRateColor = (f) => f,
+}) {
+	if (!colors.length) return <div>No Colors Listed. (Add a color)</div>;
 	return (
 		<div>
 			{colors.map((color) => (
-				<Color key={color.id} {...color} />
+				<Color
+					key={color.id}
+					{...color}
+					onRemove={onRemoveColor}
+					onRate={onRateColor}
+				/>
 			))}
 		</div>
 	);
