92a1401d1ff98399866e708a48de5a8985c1337b
jabir254 <jabirkullow@gmail.com>
2023-08-28 21:40:05 +0300

    props

--- more_react/tut/src/App.js
+++ more_react/tut/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 
 function App() {
+  const name = "Jabir"
   return (<div className="App">
     Hello world!
+    <GetName name ="Pedro" age={21} email="pedro@pedro.com" />
   </div>
   );
 }
+
+const GetName = (props) => {
+  return <h2>Pedro</h2>;
+};
+const GetNamecomponent = () => {
+  return <h1>Pedro</h1>;
+};
 
 export default App;
