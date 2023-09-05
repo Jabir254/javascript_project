fc80cdbbb9d688e7bd8a58ae2e0ca370831d54de
jabir254 <jabirkullow@gmail.com>
2023-09-05 19:40:06 +0300

    updated

--- more_react/tut/src/App.js
+++ more_react/tut/src/App.js
@@ -11,33 +11,29 @@ function App() {
   }
 
   const addTask = () => {
-    const newToDoList = [...todoList, newTask];
-    setTodoList(newToDoList);
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
   };
 
-  const deleteTask = (taskName) => {
-    const newToDoList = todoList.filter((task) => {
-      if (task === taskName){
-        return false;
-      }else {
-        return true;
-      }
-    });
-    setTodoList(newToDoList);
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
   };
 
   return (
     <div className="App">
       <div className="addTask">
         <input onChange={handleChange}/>
-        <button onClick={addTask}>Add Task</button>
+        <button  onClick={addTask}>Add Task</button>
       </div>
       <div className="list">
         {todoList.map((task) => {
           return(
           <div>
-            <h1>{task}</h1>
-            <button onClick={ () => deleteTask(task)}>Delete</button>
+            <h1>{task.taskName}</h1>
+            <button onClick={ () => deleteTask(task.id)}>Delete</button>
           </div>)
         })}
       </div>
