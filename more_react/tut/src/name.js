c4ed5505e9ac318db87e5f7136be739f87d3ead8
jabir254 <jabirkullow@gmail.com>
2023-09-09 20:57:16 +0300

    display name, age and count

--- more_react/tut/src/name.js
+++ more_react/tut/src/name.js
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 
 function Name() {
     const [name, setName] = useState("");
+    const [predictedAge, setPredictedAge] = useState(null);
     const fetchData = () => {
         Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-            console.log(res.data);
+            setPredictedAge(res.data);
         });
     };
 
@@ -19,6 +20,10 @@ function Name() {
                 }}
             />
             <button onClick={fetchData}>Predict Age</button>
+
+            <h1>Name: {predictedAge?.name}</h1>
+            <h1>Predicted Age: {predictedAge?.age}</h1>
+            <h1>count: {predictedAge?.count}</h1>
         </div>
     )
 }
