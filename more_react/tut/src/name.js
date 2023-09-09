c4a79f37f949f04e8c73a22296c369dbddca36a4
jabir254 <jabirkullow@gmail.com>
2023-09-09 20:38:46 +0300

    component that fetch data using Axios and display

---
+++ more_react/tut/src/name.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+function Name() {
+    const [name, setName] = useState("");
+    const fetchData = () => {
+        Axios.get(`https://api.agify.io/?name=${name}`).then((res) =>{
+            console.log(res.data);
+        });
+    };
+
+    return (
+        <div className="Name">
+            <input
+            placeholder="Ex. Pedro..."
+            onChange={(event) => {
+                setName(event.target.value);
+            }}
+            />
+            <button onClick={fetchData}>Predict Age</button>
+        </div>
+    )
+}
