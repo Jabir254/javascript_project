fbd340f74e461458a2cd2ba636350854d8e36599
jabir254 <jabirkullow@gmail.com>
2023-09-09 22:00:45 +0300

    added menu and contact to app file

--- more_react/tut/src/App.js
+++ more_react/tut/src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
-import { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {Home} from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Menu } from "./pages/Menu";
 
 function App() {
-return <div className="App">
-  <Router>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/" element={<Menu />} />
-    <Route path="/" element={<Contact />} />
-  </Routes>
-</Router>
+  return <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Menu />} />
+        <Route path="/" element={<Contact />} />
+      </Routes>
+    </Router>
 
-</div>
+  </div>
 }
 
 export default App;
