a6695301b3a1566974c6385a5c0365e599d65432
jabir254 <jabirkullow@gmail.com>
2023-09-10 10:57:09 +0300

    adding refetch

--- more_react/tut/src/pages/Home.js
+++ more_react/tut/src/pages/Home.js
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
-export const Home = () =>{
-    const {data, isLoading} = useQuery(["cat"], () =>{
+export const Home = () => {
+    const { data, isLoading, refetch } = useQuery(["cat"], () => {
         return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-    } );
+    });
 
     return <h1>This is the homepage <p>{data?.fact}</p></h1>
 }
