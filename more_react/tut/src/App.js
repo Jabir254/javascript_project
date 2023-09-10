6e89945b1cad7de9f746f8cd3e1f09928411e3cb
jabir254 <jabirkullow@gmail.com>
2023-09-10 09:04:30 +0300

    creating Navbar

--- more_react/tut/src/App.js
+++ more_react/tut/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { Menu } from "./pages/Menu";
+import { Navbar } from "./Navbar";
 
 function App() {
   return <div className="App">
     <Router>
+      < Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
---
+++ more_react/tut/src/Navbar.js
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <div>
+            <link to="/">Home</link>
+            <link to="/menu">Contact</link>
+            <link to="/contact">Menu</link>
+        </div>
+    )
+};
