de44720aa4cea6c232fa811d4ef70b0067884c18
jabir254 <jabirkullow@gmail.com>
2023-09-10 09:35:15 +0300

    statemanagement and useContext

--- more_react/tut/src/App.js
+++ more_react/tut/src/App.js
@@ -4,18 +4,26 @@ import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { Menu } from "./pages/Menu";
 import { Navbar } from "./Navbar";
+import { useState, useContext } from "react";
+
+export const Appcontext = createContext();
 
 function App() {
+  const [username, setUsername] = useState("JabirTech");
+
   return <div className="App">
-    <Router>
-      < Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<h1>404 page not found</h1>} />
-      </Routes>
-    </Router>
+    <Appcontext.Provider value={{username, setUsername}}>
+      <Router>
+        < Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>404 page not found</h1>} />
+        </Routes>
+      </Router>
+    </Appcontext.Provider>
+
 
   </div>
 }
--- more_react/tut/src/pages/Home.js
+++ more_react/tut/src/pages/Home.js
@@ -1,3 +1,6 @@
+import { useContext } from "react"
+import { Appcontext } from "../App"
 export const Home = () => {
-    return <h1>This is the home page</h1>
+    const {username} = useContext(Appcontext);
+    return <h1>This is the home page and the user is: {username}</h1>
 }
