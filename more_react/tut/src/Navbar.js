4bfb29f82f2aac13a96c372c4563735b66a75cbe
jabir254 <jabirkullow@gmail.com>
2023-09-10 09:09:22 +0300

    added navbar

--- more_react/tut/src/Navbar.js
+++ more_react/tut/src/Navbar.js
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 export const Navbar = () => {
     return (
         <div>
-            <link to="/">Home</link>
-            <link to="/menu">Contact</link>
-            <link to="/contact">Menu</link>
+            <Link to="/"> Home </Link>
+            <Link to="/menu" >Menu </Link>
+            <Link to="/contact" >Contact</Link>
         </div>
-    )
+    );
 };
